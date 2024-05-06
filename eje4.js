62382cd167cd573de38eee6a65ef9c0d0d8fa4a0
AdrianGH28 <160188260+AdrianGH28@users.noreply.github.com>
2024-05-06 10:07:13 -0600

    eje4.js

--- eje4.js
+++ eje4.js
@@ -1,12 +1,23 @@
-document.getElementById('form').addEventListener('submit', function(event) {
+document.getElementById('form').addEventListener('submit', function (event) {
     event.preventDefault();
-    var parcial1 = parseFloat(document.getElementById('cal1').value);
-    var parcial2 = parseFloat(document.getElementById('cal2').value);
-    var parcial3 = parseFloat(document.getElementById('cal3').value);
-    var examenFinal = parseFloat(document.getElementById('examen').value);
-    var trabajoFinal = parseFloat(document.getElementById('trabajo').value);
-    var promedioParciales = (parcial1 + parcial2 + parcial3) / 3;
+    var cal1Input = document.getElementById('cal1');
+    var cal1 = parseFloat(cal1Input.value);
+    var cal2Input = document.getElementById('cal2');
+    var cal2 = parseFloat(cal2Input.value);
+    var cal3Input = document.getElementById('cal3');
+    var cal3 = parseFloat(cal3Input.value);
+    var examenInput = document.getElementById('examen');
+    var examenFinal = parseFloat(examenInput.value);
+    var trabajoInput = document.getElementById('trabajo');
+    var trabajoFinal = parseFloat(trabajoInput.value);
+
+    if (isNaN(cal1) || cal1 < 0 || cal1 > 100 || isNaN(cal2) || cal2 < 0 || cal2 > 100 || isNaN(cal3) || cal3 < 0 || cal3 > 100 || isNaN(examenFinal) || examenFinal < 0 || examenFinal > 100 || isNaN(trabajoFinal) || trabajoFinal < 0 || trabajoFinal > 100) {
+        alert("Por favor, ingrese números positivos menores o iguales a 100 para todos los campos.");
+        return;
+    }
+
+    var promedioParciales = (cal1 + cal2 + cal3) / 3;
     var calificacionFinal = (promedioParciales * 0.55) + (examenFinal * 0.30) + (trabajoFinal * 0.15);
 
-    document.getElementById('resultado').innerText = "La calificación final del alumno es " + calificacionFinal;
+    document.getElementById('resultado').innerText = "La calificación final del alumno es " + calificacionFinal.toFixed(2);
 });
