a63e20ec38c2fe5935d36d5650a9857faa841cf6
AdrianGH28 <160188260+AdrianGH28@users.noreply.github.com>
2024-05-06 10:06:36 -0600

    eje3.js

--- eje3.js
+++ eje3.js
@@ -1,7 +1,17 @@
 document.getElementById("form").addEventListener('submit', function(event) {
     event.preventDefault();
-    var precion = parseFloat(document.getElementById('inp').value);
+    var precioInput = document.getElementById('inp');
+    var precio = parseFloat(precioInput.value);
+
+ 
+    if (isNaN(precio) || precio <= 0) {
+        alert("Por favor, ingrese un valor de precio positivo");
+        precioInput.focus();
+        return;
+    }
+
     var descuento = 0.15;
-    var total = precion - (precion * descuento);
-    document.getElementById('resultado').innerText = "El precio con el descuento es de $" + total;
+    var total = precio - (precio * descuento);
+
+    document.getElementById('resultado').innerText = "El precio con el descuento es de $" + total.toFixed(2);
 });
