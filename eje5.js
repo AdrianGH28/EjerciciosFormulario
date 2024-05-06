e6e31195472a988f877ab7a1e03022cc124fecc6
AdrianGH28 <160188260+AdrianGH28@users.noreply.github.com>
2024-05-06 10:07:57 -0600

    eje5.js

--- eje5.js
+++ eje5.js
@@ -1,10 +1,24 @@
 document.getElementById('form').addEventListener('submit', function(event) {
     event.preventDefault();
-    var estudiantes = parseFloat(document.getElementById('estudiantes').value);
-    var hombres = parseFloat(document.getElementById('hom').value);
-    var mujeres = parseFloat(document.getElementById('muj').value);
-    var porcentajeHombres = (hombres/estudiantes)*100;
-    var porcentajeMujeres = (mujeres/estudiantes)*100;
+    var estudiantesInput = document.getElementById('estudiantes');
+    var estudiantes = parseFloat(estudiantesInput.value);
+    var hombresInput = document.getElementById('hom');
+    var hombres = parseFloat(hombresInput.value);
+    var mujeresInput = document.getElementById('muj');
+    var mujeres = parseFloat(mujeresInput.value);
 
-    document.getElementById('resultado').innerText = "El porcentaje de hombres es de " + porcentajeHombres.toFixed(2) + "% y de mujeres es de " + porcentajeMujeres.toFixed(2) + "%";
+    if (isNaN(estudiantes) || estudiantes <= 0 || isNaN(hombres) || hombres < 0 || isNaN(mujeres) || mujeres < 0) {
+        alert("Por favor, ingrese números positivos para todos los campos.");
+        return;
+    }
+
+    if (estudiantes !== hombres + mujeres) {
+        alert("El número total de estudiantes debe ser igual a la suma de la cantidad de estudiantes");
+        return;
+    }
+
+    var porcentajeHombres = (hombres / estudiantes) * 100;
+    var porcentajeMujeres = (mujeres / estudiantes) * 100;
+
+    document.getElementById('resultado').innerText = "El porcentaje de hombres es de " + porcentajeHombres.toFixed(2) + "% y el de mujeres es de " + porcentajeMujeres.toFixed(2) + "%";
 });
